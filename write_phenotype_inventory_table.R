d873ab150da72611404d7e531e19c8f37194162b
Adrienne Stilp <amstilp@uw.edu>
2024-04-08 16:07:16 -0700

    Update script to use argparser

--- write_phenotype_inventory_table.R
+++ write_phenotype_inventory_table.R
@@ -1,23 +1,37 @@
+library(argparser)
 library(AnVIL)
 library(AnvilDataModels)
-library(tidyverse)
+library(dplyr)
+library(readr)
+library(tidyr)
 
-# List of workspaces - this is an input.
-workspaces <- c(
-  "primed-data-prevent-1/PRIMED_ARIC_DBGAP_PHS000280_V8_P2_HMB-IRB",
-  "primed-data-dprism-1/PRIMED_RPGEH_DBGAP_PHS000788_V2_P3_HMB-IRB-NPU",
-  "primed-data-topmed-1/PRIMED_CARDIA_TOPMED_DBGAP_PHS001612_V1_P1_HMB-IRB",
-  NULL
-)
 
-get_phenotype_inventory_table_for_workspace <- function(workspace) {
+argp <- arg_parser("write_phenotype_inventory_table.R", description="Write phenotype inventory table for shared workspaces.")
+argp <- add_argument(argp, "--workspaces-file", help="2-column tsv file with (workspace, studies)")
+argp <- add_argument(argp, "--output-workspace-namespace", help="Namespace of the AnVIL workspace to write the phenotype inventory table to.")
+argp <- add_argument(argp, "--output-workspace-name", help="Name of the AnVIL workspace to write the phenotype inventory table to.")
+argp <- add_argument(argp, "--output-table-name", help="Name of the data table to write the phenotype inventory table to.")
+argv <- parse_args(argp)
+
+# Read in the workspaces.
+workspaces <- read_tsv(argv$workspaces_file, col_names=c("workspace", "studies"))
+# workspaces <- tribble(
+#   ~workspace, ~studies,
+#   "primed-data-prevent-1/PRIMED_ARIC_DBGAP_PHS000280_V8_P2_HMB-IRB", "ARIC",
+#   "primed-data-dprism-1/PRIMED_RPGEH_DBGAP_PHS000788_V2_P3_HMB-IRB-NPU", "GERA, RPGEH",
+#   "primed-data-topmed-1/PRIMED_CARDIA_TOPMED_DBGAP_PHS001612_V1_P1_HMB-IRB", "CARDIA"
+# )
+
+# Loop over workspaces and pull the phenotype inventory information.
+results_list <- list()
+for (workspace in workspaces$workspace) {
   table_name <- "phenotype_harmonized"
 
   workspace_namespace = str_split_1(workspace, pattern="/")[1]
   workspace_name = str_split_1(workspace, pattern="/")[2]
   tables <- avtables(namespace=workspace_namespace, name=workspace_name)
   if (table_name %in% tables$table) {
-    table <- avtable("phenotype_harmonized", namespace=workspace_namespace, name=workspace_name)
+    table <- avtable(table_name, namespace=workspace_namespace, name=workspace_name)
     x <- table %>%
       # phenotype_harmonized_id is needed to make the table unique.
       select(phenotype_harmonized_id, table=domain, n_subjects, n_rows, file_path)
@@ -25,40 +39,34 @@ get_phenotype_inventory_table_for_workspace <- function(workspace) {
   else {
     x = tibble()
   }
-  return(x)
+  results_list[[workspace]] <- x
 }
 
-# Loop over workspaces and get phenotype_harmonized table.
-
-results_list <- list()
-
-workspace <- workspaces[1]
-for (workspace in workspaces) {
-  results_list[[workspace]] <- get_phenotype_inventory_table_for_workspace(workspace)
-}
-
+# Combine the results into a single data frame.
 results <- bind_rows(results_list, .id="workspace") %>%
+  left_join(workspaces, by="workspace") %>%
   rename(
     phenotype_inventory_id=phenotype_harmonized_id
   ) %>%
-  select(phenotype_inventory_id, everything())
+  select(phenotype_inventory_id, studies, everything())
 
 # Set up output workspace info.
-output_workspace = avworkspace() # This will be different when we actually run the script.
-output_workspace_namespace = str_split_1(output_workspace, pattern="/")[1]
-output_workspace_name = str_split_1(output_workspace, pattern="/")[2]
+# output_workspace = avworkspace() # This will be different when we actually run the script.
+# output_workspace_namespace = str_split_1(output_workspace, pattern="/")[1]
+# output_workspace_name = str_split_1(output_workspace, pattern="/")[2]
+output_workspace_namespace = argv$output_workspace_namespace
+output_workspace_name = argv$output_workspace_name
+output_table_name = argv$output_table_name
 
 # Get the original results
-table_name <- "phenotype_inventory"
 tables <- avtables(namespace=output_workspace_namespace, name=output_workspace_name)
 if (table_name %in% tables$table) {
-  original_results <- avtable(table_name, namespace=output_workspace_namespace, name=output_workspace_name)
-  avtable_delete_values(table_name, original_results$phenotype_inventory_id)
+  avtable_delete_values(output_table_name, original_results$phenotype_inventory_id)
 }
 
-# Write the new results the table.
-# Note: anvil_import_Tables will check job status and timeout after an hour (by default).
-anvil_import_tables(list(phenotype_inventory=results), namespace=output_workspace_namespace, name=output_workspace_name, overwrite=TRUE)
+tables <- list(tmp=results)
+setNames(tables, output_table_name)
 
-# Delete any rows from the original table that weren't in the new table.
-(job_status <- avtable_import_status(job_status))
+# Write the new results the table.
+# Note: anvil_import_tables will check job status and timeout after an hour (by default).
+anvil_import_tables(tables, namespace=output_workspace_namespace, name=output_workspace_name, overwrite=TRUE)
