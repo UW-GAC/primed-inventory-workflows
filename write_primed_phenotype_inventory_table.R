71e90bbc6b9f615e6d5a3e520d4ef1c9d23a813d
Adrienne Stilp <amstilp@uw.edu>
2024-04-09 15:12:24 -0700

    Fix typo

--- write_primed_phenotype_inventory_table.R
+++ write_primed_phenotype_inventory_table.R
@@ -74,7 +74,7 @@ results <- results %>%
 
 # Delete the table before writing the new data, if it already exists.
 tables <- avtables(namespace=output_workspace_namespace, name=output_workspace_name)
-if output_table_name %in% tables$table) {
+if (output_table_name %in% tables$table) {
   original_results <- avtable(output_table_name, namespace=output_workspace_namespace, name=output_workspace_name)
   avtable_delete_values(output_table_name, original_results$phenotype_inventory_id, namespace=output_workspace_namespace, name=output_workspace_name)
 }
